48313e58692223c1798be653e4853f2c549663f3
JoyceAja <ajagbe.joyce@gmail.com>
2018-11-06 19:57:05 -0500

    adding the drag effect

--- src/pages/ColorDisplay.js
+++ src/pages/ColorDisplay.js
@@ -4,22 +4,29 @@ import styles from "./container.module.css";
 let colors = ["red", "blue", "purple", "orange", "green", "yellow"];
 //It is actually going to be displayed as a color image
 
-export default () => (
-  <div>
-    <ul className="container">
-      {colors.map((color, idx) => (
-        <div
-          key={idx}
-          draggable
-          className={styles.draggable}
-          style={{
-            backgroundColor: `${color}`,
-            padding: `15px`,
-            width: `200px`
-          }}
-        >
-        </div>
-      ))}
-    </ul>
-  </div>
-);
+export default class Color extends Component {
+    onDragStart = (e, id) => {
+        e.dataTransfer.setData("id", id)
+    }
+  render() {
+    return (
+      <div>
+        <ul className="container">
+          {colors.map((color, idx) => (
+            <div
+              key={idx}
+              draggable
+              onDragStart={(e) => this.onDragStart(e, idx)}
+              className={styles.draggable}
+              style={{
+                backgroundColor: `${color}`,
+                padding: `15px`,
+                width: `200px`
+              }}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
